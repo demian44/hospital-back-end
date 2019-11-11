2f7ce4c6266e73383acd9fd408a7de95edd90049
900005190 <Martin.Vasconcelos@cordialfinanciera.com.ar>
2019-11-11 16:41:19 -0300

    Agrego logica para validar google token en el backend

--- config/config.js
+++ config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = '@este-es@-un-seed-dificil';
+
+//GOOGLE
+module.exports.CLIENT_ID = '603513800589-hahn8ucmbu3ivui9qlrta7od186a77ao.apps.googleusercontent.com';
--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,13 @@ var User = require('../models/user');
 var jwt = require('jsonwebtoken');
 var SEED = require('../config/config').SEED;
 
+
 app = express();
+
+//GOOGLE
+var CLIENT_ID = require('../config/config').CLIENT_ID;
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
 
 app.post('/', (request, response) => {
 
@@ -38,7 +44,7 @@ app.post('/', (request, response) => {
       });
     }
 
-    userResponse = { name: userResponse.name, email: userResponse.email, id: userResponse._id }                
+    userResponse = { name: userResponse.name, email: userResponse.email, id: userResponse._id }
     let token = jwt.sign({ user: userResponse }, SEED, { expiresIn: 1014400 });
     response.status(200).json({
       ok: true,
@@ -49,15 +55,44 @@ app.post('/', (request, response) => {
   });
 });
 
-app.post('/google',(req, res)=>{
+
+async function verify(token) {
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+    // Or, if multiple clients access the backend:
+    //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+
+  const payload = ticket.getPayload();
+  const userid = payload['sub'];
+  // If request specified a G Suite domain:
+  //const domain = payload['hd'];
+
+  return {
+    name: payload.name,
+    google: true,
+    email: payload.email,
+    img: payload.picture
+  }
+}
+
+app.post('/google', async (req, res) => {
+  let token = req.body.token;
+  var googleUser = await verify(token).catch(error => {
+    return res.status(403).json({
+      ok: false,
+      message: 'Token de goolge inválido',
+      error: error,
+    });
+  });
+
 
   return res.status(200).json({
     ok: true,
     message: 'Todo Piola',
-    test
+    googleUser: googleUser
   })
-
 });
-
 
 module.exports = app;
